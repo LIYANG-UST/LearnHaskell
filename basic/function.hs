7e11cc9a28232258a795d7b747a933e6c59b7fdc
LIYang <liyangTJU@outlook.com>
2021-10-01 12:21:40 +0800

    ss

--- basic/function.hs
+++ basic/function.hs
@@ -2,3 +2,24 @@ lucky :: (Integral a) => a -> String
 lucky 8 = "lucky number eight"
 lucky 11 = "hello"
 lucky x = "sorry not correct"
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe x = "Wrong!"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
